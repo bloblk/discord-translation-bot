c3256ffb8033170b20ac90873a39515b6e452a50
bloblk <153786531+bloblk@users.noreply.github.com>
2024-02-05 19:45:22 -0800

    Add files via upload

---
+++ discord-translate-bot.py
@@ -0,0 +1,160 @@
+import discord
+from googletrans import Translator
+
+
+bot_token = 'MTE5NTg0MzIyODg2MDE2MjE5NA.GrnCZs.eo9Kc25fOQBt1-Jg6TViPFmCDJ1wLjtrQRk37w'
+translator = Translator()
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+languages = ['en']
+toggle = True
+
+langDict = {
+    'afrikaans': 'af',
+    'albanian': 'sq',
+    'amharic': 'am',
+    'arabic': 'ar',
+    'armenian': 'hy',
+    'azerbaijani': 'az',
+    'basque': 'eu',
+    'belarusian': 'be',
+    'bengali': 'bn',
+    'bosnian': 'bs',
+    'bulgarian': 'bg',
+    'catalan': 'ca',
+    'cebuano': 'ceb',
+    'chichewa': 'ny',
+    'chinese-simplified': 'zh-cn',
+    'chinese-traditional': 'zh-tw',
+    'corsican': 'co',
+    'croatian': 'hr',
+    'czech': 'cs',
+    'danish': 'da',
+    'dutch': 'nl',
+    'english': 'en',
+    'esperanto': 'eo',
+    'estonian': 'et',
+    'filipino': 'tl',
+    'finnish': 'fi',
+    'french': 'fr',
+    'frisian': 'fy',
+    'galician': 'gl',
+    'georgian': 'ka',
+    'german': 'de',
+    'greek': 'el',
+    'gujarati': 'gu',
+    'haitian creole': 'ht',
+    'hausa': 'ha',
+    'hawaiian': 'haw',
+    'hebrew': 'he',
+    'hindi': 'hi',
+    'hmong': 'hmn',
+    'hungarian': 'hu',
+    'icelandic': 'is',
+    'igbo': 'ig',
+    'indonesian': 'id',
+    'irish': 'ga',
+    'italian': 'it',
+    'japanese': 'ja',
+    'javanese': 'jw',
+    'kannada': 'kn',
+    'kazakh': 'kk',
+    'khmer': 'km',
+    'korean': 'ko',
+    'kurdish-kurmanji': 'ku',
+    'kyrgyz': 'ky',
+    'lao': 'lo',
+    'latin': 'la',
+    'latvian': 'lv',
+    'lithuanian': 'lt',
+    'luxembourgish': 'lb',
+    'macedonian': 'mk',
+    'malagasy': 'mg',
+    'malay': 'ms',
+    'malayalam': 'ml',
+    'maltese': 'mt',
+    'maori': 'mi',
+    'marathi': 'mr',
+    'mongolian': 'mn',
+    'myanmar': 'my',
+    'nepali': 'ne',
+    'norwegian': 'no',
+    'odia': 'or',
+    'pashto': 'ps',
+    'persian': 'fa',
+    'polish': 'pl',
+    'portuguese': 'pt',
+    'punjabi': 'pa',
+    'romanian': 'ro',
+    'russian': 'ru',
+    'samoan': 'sm',
+    'scots gaelic': 'gd',
+    'serbian': 'sr',
+    'sesotho': 'st',
+    'shona': 'sn',
+    'sindhi': 'sd',
+    'sinhala': 'si',
+    'slovak': 'sk',
+    'slovenian': 'sl',
+    'somali': 'so',
+    'spanish': 'es',
+    'sundanese': 'su',
+    'swahili': 'sw',
+    'swedish': 'sv',
+    'tajik': 'tg',
+    'tamil': 'ta',
+    'telugu': 'te',
+    'thai': 'th',
+    'turkish': 'tr',
+    'ukrainian': 'uk',
+    'urdu': 'ur',
+    'uyghur': 'ug',
+    'uzbek': 'uz',
+    'vietnamese': 'vi',
+    'welsh': 'cy',
+    'xhosa': 'xh',
+    'yiddish': 'yi',
+    'yoruba': 'yo',
+    'zulu': 'zu'
+}
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    global languages
+    global toggle
+
+    if message.author == client.user:
+        return
+
+    msgSplit = message.content.split(" ")
+    if (msgSplit[0] == "$setLang"):
+        languages = []
+        for language in msgSplit[1:]:
+            try:
+                languages.append(langDict[language])
+                await message.channel.send(
+                    f'Added destination language {language}')
+            except KeyError:
+                await message.channel.send(
+                    f'{language} is not a valid language')
+    elif (msgSplit[0] == "$toggle"):
+        toggle = not toggle
+    elif toggle:
+        for lang in languages:
+            translated_text = translator.translate(message.content, dest=lang).text
+            await message.channel.send(translated_text)
+
+
+# Discord Bot
+client.run(bot_token)
